b457e407a6bcbb48ea1b07e3c5a417c994a2af49
TakashiHirota <35054232+TakashiHirota@users.noreply.github.com>
2018-03-12 12:10:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from socket import gethostbyname
 
+#function for getting ip
 def ip(url):
     return gethostbyname(url)
 
 while True:
-    u = input("enter website: ")
-    print(gethostbyname(u))
+    u = input("enter website: ") #address here
+    print(gethostbyname(u)) #prints IPv4
     
 #website url cannot have https/http, can't have : or ::, nor / or //. just website.com/org/whatever
