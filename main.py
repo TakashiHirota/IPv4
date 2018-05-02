54a3d7682fa187bd15f25f8f3ee4f897037fc6d2
ocean <35054232+thedeepocean@users.noreply.github.com>
2018-05-01 17:10:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ while True:
     u = input("enter website: ") #address here
     print(gethostbyname(u)) #prints IPv4
     
-#website url cannot have https/http, can't have : or ::, nor / or //. just website.com/org/whatever
+#website url cannot have https/http, can't have : or ::, nor / or //. just website.com.org/whatever
